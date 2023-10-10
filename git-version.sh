0b5a4841689df84e45a20e6157891bd3f8c29e20
M.E.L <mel@rustbelt>
2023-10-10 15:55:02 +0200

    Enrich basic version label with DIRTY and COMMIT info

--- git-version.sh
+++ git-version.sh
@@ -1,8 +1,25 @@
+# BEN - Build Everything Now
+# Copyright (C) 2023  M E Leypold
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 cd "$(dirname "$(realpath "$0")")"
 if test "$(git status -s | wc -l)" -gt 0; then
-    _DIRTY="+""$(date -Is | sed 's|[+].*||;s|[-T:]||g')"
+    _DIRTY="+DIRTY-""$(date -Is | sed 's|[+].*||;s|[-T:]||g')"
 else
     _DIRTY=""
 fi
 
-echo "$(git describe --tags | sed 's|-g.*||g;')"$_DIRTY
+echo "$(git describe --tags | sed 's|-g\(.*\)|+COMMIT-\1|g;')"$_DIRTY
