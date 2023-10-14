68c041a48d620a8be95532934a5f45961056eb2d
M.E.L <mel@rustbelt>
2023-10-14 12:30:36 +0200

    ben: Add ben upstreaming targets

---
+++ ben.mk
@@ -0,0 +1,37 @@
+# BEN - Build Everything Now
+# Copyright (C) 2023  M E Leypold
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ifdef   BEN-RULE-SET
+
+BEN:
+	: Create BEN worktree from local ben branch
+	$(SET-SH)
+	git worktree add BEN ben/$(BEN-RULE-SET)
+	:
+
+ben-split+merge: BEN
+	: Split off .ben and merge to BEN
+	$(SET-SH)
+	$(BEN-COMMON)/ben-split+merge .ben BEN
+	:
+
+ben-upstream: ben-split+merge BEN
+	: Push ben branch to upstream
+	$(SET-SH)
+	git push BEN ben/$(BEN-RULE-SET)
+	:
+
+endif
